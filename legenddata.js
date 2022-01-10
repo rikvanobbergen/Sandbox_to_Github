c9b53f3e97743e98c099335346b5543c2a544389
rikvanobbergen <95224880+rikvanobbergen@users.noreply.github.com>
2022-01-10 13:44:19 +0100

    Initial commit

---
+++ legenddata.js
@@ -0,0 +1,67 @@
+export const legenddata = [
+  {
+    id: "Abomination",
+    id2: "Assassin",
+    colour: "#ff0000"
+  },
+  {
+    id: "Cruel",
+    id2: "Brawler",
+    colour: "#8b0000"
+  },
+  {
+    id: "Dawnbringer",
+    id2: "Caretaker",
+    colour: "#ff8c00"
+  },
+  {
+    id: "Draconic",
+    id2: "Cannonneers",
+    colour: "#90ee90"
+  },
+  {
+    id: "Forgotten",
+    id2: "Cavalier",
+    colour: "#d3d3d3"
+  },
+  {
+    id: "Hellion",
+    id2: "Invoker",
+    colour: "#ffff00"
+  },
+  {
+    id: "Inanimate",
+    id2: "Knight",
+    colour: "#228b22"
+  },
+  {
+    id: "Ironclad",
+    id2: "Legionnaire",
+    colour: "#808080"
+  },
+  {
+    id: "Nightbringer",
+    id2: "Mystic",
+    colour: "#000080"
+  },
+  {
+    id: "Revenant",
+    id2: "Ranger",
+    colour: "#8b4513"
+  },
+  {
+    id: "Redeemed",
+    id2: "Renewer",
+    colour: "#87cefa"
+  },
+  {
+    id: "Sentinel",
+    id2: "Skirmisher",
+    colour: "#8b008b"
+  },
+  {
+    id: "Victorious",
+    id2: "Spellweaver",
+    colour: "#0000cd"
+  }
+];
